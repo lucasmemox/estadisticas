f9f726e03153d6a35d63f898983a19cc90c90caf
lucasmemox <memo.bentancour@gmail.com>
2025-06-26 14:45:50 -0300

    dashboard link tarjeta docxcomision

--- usuario/urls.py
+++ usuario/urls.py
@@ -69,10 +69,11 @@ urlpatterns = [
     # **FIN RESULTADO DE CURSADAS REPORTES**
     ######################################################################################################
 
-    # **NUEVA URL PARA RESULTADO DE CURSADAS**
+    # **NUEVA URL PARA DOCENTES POR COMISION**
     path('reportes/docentes/', views.docentes_x_comision_report, name='docentes_x_comision_report'),
+
     ######################################################################################################
-    # **FIN RESULTADO DE CURSADAS REPORTES**
+    # **FIN RDOCENTES POR COMISION**
     ######################################################################################################
 
     ########################################################################
